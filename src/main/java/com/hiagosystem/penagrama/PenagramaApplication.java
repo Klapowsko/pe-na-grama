265dd4fc810dc715cd08789ac77c3ac071e522f4
Hiago Klapowsko <hiagoklapowsko@gmail.com>
2026-09-03 15:48:14 -0300

    Initialize Spring Boot project with Gradle setup and essential configurations

---
+++ src/main/java/com/hiagosystem/penagrama/PenagramaApplication.java
@@ -0,0 +1,13 @@
+package com.hiagosystem.penagrama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenagramaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenagramaApplication.class, args);
+	}
+
+}
